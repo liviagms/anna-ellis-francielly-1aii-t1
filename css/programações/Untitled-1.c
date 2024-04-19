521dfb378f65d6eec2a6767501173ceb48d31cc6
liviagms <magalhaesannalivia7@gmail.com>
2024-04-19 14:46:54 -0300

    .

---
+++ css/programações/Untitled-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    float a,p,r,r2;
+    printf ("digite seu raio: ");
+    scanf ("%f",&r);
+    r2 = r*r;
+    p = 2*3.14*r;
+    a = 3.14*r2;
+    printf("seu perimetro e %f", a);
+
+
